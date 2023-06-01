336da2c5d1f6d650fa223def8b9d231bf424e0d2
unknown <augustomonteirodelucena@gmail.com>
2023-06-01 15:59:00 -0300

    listas python

---
+++ lista_tuplas/arquivo2.py
@@ -0,0 +1,12 @@
+# Questão 2 - Dicionário de números favoritos
+
+numeros_favoritos = {
+    "Leticia": 5,
+    "José": 17,
+    "Augusto": 17,
+    "Roberta": 37,
+    "Carol": 39
+}
+
+for pessoa, numero in numeros_favoritos.items():
+    print(pessoa + ":", numero)
